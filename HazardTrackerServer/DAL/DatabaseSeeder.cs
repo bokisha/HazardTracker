86bd557d8dd6a92788ab7363a11213c35b24b700
musasabi333 <musasabi333@gmail.com>
2020-04-10 18:44:29 +0200

    Added to seeders

--- HazardTrackerServer/DAL/DatabaseSeeder.cs
+++ HazardTrackerServer/DAL/DatabaseSeeder.cs
@@ -24,16 +24,25 @@ namespace DAL
             _hazardTrackerDbContext.Database.EnsureDeleted();
             _hazardTrackerDbContext.Database.EnsureCreated();
 
-            var user = new UserEntity {Imei = "ooooo", IsInfected = false};
-            _userRepository.Create(user);
+            var user1 = new UserEntity { Imei = "ooooo", IsInfected = false };
+            _userRepository.Create(user1);
 
-            var location1 = new LocationEntity {Name = "Maxi", Address = "Ulica1", Latitude = 0, Longitude = 0};
+            var user2 = new UserEntity { Imei = "lllll", IsInfected = false };
+            _userRepository.Create(user2);
+
+            var location1 = new LocationEntity { Name = "Maxi", Address = "Džona Kenedija 10, Beograd 11080, Serbia", Latitude = 132462513, Longitude = 456758652 };
             _locationRepository.Create(location1);
 
-            var location2 = new LocationEntity { Name = "Tempo", Address = "Ulica2", Latitude = 0, Longitude = 0 };
+            var location2 = new LocationEntity { Name = "Tempo", Address = "Bulevar Mihajla Pupina 181v, Novi Beograd 11070, Serbia", Latitude = 21324562132, Longitude = 456756514 };
             _locationRepository.Create(location2);
 
-            var visitation1 = new VisitationEntity {Imei = "ooooo", Location = location1};
+            var location3 = new LocationEntity { Name = "Maxi", Address = "Narodnih heroja 30, Beograd 11070, Serbia", Latitude = 21324562132, Longitude = 456756513 };
+            _locationRepository.Create(location3);
+
+            var location4 = new LocationEntity { Name = "Idea", Address = "Bulevar Mihajla Pupina 181v, Novi Beograd 11070, Serbia", Latitude = 21324562132, Longitude = 456756513 };
+            _locationRepository.Create(location4);
+
+            var visitation1 = new VisitationEntity { Imei = "ooooo", Location = location1 };
             _visitationRepository.Create(visitation1);
 
             var visitation2 = new VisitationEntity { Imei = "lllll", Location = location2 };
